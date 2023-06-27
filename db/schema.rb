3a6b5798516feae44593fe135d52e0bc0766b4d1
zeff96 <zeffo1975@gmail.com>
2023-06-27 16:32:28 +0300

    Update data types for recipe columns

--- db/schema.rb
+++ db/schema.rb
@@ -52,8 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_091612) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.time "preparation_time"
-    t.time "cooking_time"
+    t.string "preparation_time"
+    t.string "cooking_time"
     t.text "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
