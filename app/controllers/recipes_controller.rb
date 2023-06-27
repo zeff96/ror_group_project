d9d86b23cc1833bdcc4b25b9d12c401af5aebca5
zeff96 <zeffo1975@gmail.com>
2023-06-27 20:32:37 +0300

    Add align-center for add new recipe title

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,28 @@ class RecipesController < ApplicationController
 	def show
 		@recipe = Recipe.find(params[:id])
 	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+
+		respond_to do |format|
+			if @recipe.save
+				format.html { redirect_to recipe_path, notice: 'Recipe created succesfully!' }
+				format.json { render :show, status: :created, location: @recipe }
+			else
+				format.html { redirect_to new_recipe_path, alert: 'Recipe not created!' }
+				format.json { render json: @recipe.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:name, :cooking_time, :preparation_time, :description, :status)
+	end
 end
