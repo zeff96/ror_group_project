f4c0e57a6fc1e923d12aa46913ffe7a25d64ce03
zeff96 <zeffo1975@gmail.com>
2023-06-27 19:33:49 +0300

    Add recipe validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,11 @@
 class Recipe < ApplicationRecord
+  include Visible
+
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, presence: true
+  validates :cooking_time, presence: true
+  validates :preparation_time, presence: true
+  validates :description, presence: true
 end
