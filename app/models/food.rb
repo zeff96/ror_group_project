420073ab5fb146a35540a45bab301d80b5ce64e1
QueenterJuma <jumahqueenter@gmail.com>
2023-06-29 06:20:21 -0700

    Modify food.rb

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,6 @@
 class Food < ApplicationRecord
-	belongs_to :user
-	has_many :inventory_foods
-    has_many :inventories, through: :inventory_foods
-    has_many :recipe_foods
-    has_many :recipes, through: :recipe_foods
+  has_many :inventory_foods
+  has_many :inventories, through: :inventory_foods
+  has_many :recipe_foods
+  has_many :recipes, through: :recipe_foods
 end
