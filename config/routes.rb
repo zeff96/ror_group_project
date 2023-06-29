1304f6b8af6e7eccb54632a66f861d66b1cf0fc3
QueenterJuma <jumahqueenter@gmail.com>
2023-06-29 06:22:38 -0700

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
 
   devise_scope :user do
     authenticated :user do
-      #root to: "devise/sessions#new", as: :authenticated_root
+      root to: 'foods#index', as: :authenticated_root
     end
 
     unauthenticated :user do
-      root to: "devise/sessions#new", as: :authenticated_root
+      root to: "devise/sessions#new", as: :unauthenticated_root
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :foods
+  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
