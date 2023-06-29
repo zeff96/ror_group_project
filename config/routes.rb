758b030215f7f4c50626e0596daf3d17595a7081
zeff96 <zeffo1975@gmail.com>
2023-06-29 21:56:19 +0300

    Update root path to inventories

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root to: 'foods#index', as: :authenticated_root
+      root to: 'inventories#index', as: :authenticated_root
     end
 
     unauthenticated :user do
