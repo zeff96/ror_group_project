4a0e4a9d16868f5d0765cb0bc47215dd1630c253
zeff96 <zeffo1975@gmail.com>
2023-06-30 22:34:36 +0300

    Update validation status

--- spec/model/food_spec.rb
+++ spec/model/food_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe Food, type: :model do
   before :each do
-    @food = Food.create(name: 'Jolof', measurement_unit: 3, price: 1)
+    @food = Food.create(name: 'Jolof', measurement_unit: 3, price: 1, unit_quantity: 'kgs')
   end
   it 'should have a name' do
     food = @food.name
--- spec/model/recipe_food_spec.rb
+++ spec/model/recipe_food_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Recipe', type: :model do
   before :each do
     @user = User.create(name: 'Test', email: 'test@example.com', password: 'password')
     @recipe = Recipe.create(user: @user, name: 'test recipe', preparation_time: '1 hr', cooking_time: '1.5 hrs',
-                            description: 'test test')
+                            description: 'test test', public: true)
     @test_food = Food.create(name: 'test food', measurement_unit: 2, price: 10, unit_quantity: 'kgs')
     @recipe_food = RecipeFood.create(quantity: 10, food: @test_food, recipe: @recipe)
   end
